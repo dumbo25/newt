38f8a23c8a7fa43ccd38a9c45a109c2c3e7a43b9
dumbo25 <dumbo25@users.noreply.github.com>
2021-10-07 17:30:33 -0500

    Update run_app.sh

--- system/run_app.sh
+++ system/run_app.sh
@@ -4,12 +4,3 @@ sudo /usr/bin/python3 server.py
 
 # Allow Cherrypy to boot up
 sleep 5
-
-# Run on Desktop or headless
-#   run headless ???
-#   ??? what needs to be done for headless ???
-
-#   run on Raspberry Pi Desktop
-#     don't run script to remove unusued stuff in Raspberry Pi setup insturctions ???
-#     uncomment following line
-# chromium-browser --app=http://hostname:port
