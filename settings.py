9bb3cb5da9736f4c515ce87399bd0fcdbe9c97c7
dumbo25 <dumbo25@users.noreply.github.com>
2021-11-04 19:23:38 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -129,8 +129,9 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # STATIC_ROOT = os.path.join( BASE_DIR, "static/")
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
